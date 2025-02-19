39fad846f4f94e7b2375a1b5974a9abe111b659a
kkkkimtaehyeon <kth1927@naver.com>
2025-02-19 20:19:10 +0900

    s3 파일 업로드 구현

--- src/main/java/com/kth/aibook/service/cloud/CloudStorageService.java
+++ src/main/java/com/kth/aibook/service/cloud/CloudStorageService.java
@@ -2,8 +2,11 @@ package com.kth.aibook.service.cloud;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface CloudStorageService {
     String uploadFile(MultipartFile file);
+    String uploadFiles(List<MultipartFile> files);
     String getFile(String fileName);
     void removeFile(String fileName);
 }
--- src/main/java/com/kth/aibook/service/cloud/impl/CloudStorageServiceImpl.java
+++ src/main/java/com/kth/aibook/service/cloud/impl/CloudStorageServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -25,6 +26,11 @@ public class CloudStorageServiceImpl implements CloudStorageService {
         String fileName = getUniqueFileName(multipartFile);
         putObject(multipartFile, fileName);
         return fetchUrl(fileName);
+    }
+
+    @Override
+    public String uploadFiles(List<MultipartFile> files) {
+        return "";
     }
 
     @Override
