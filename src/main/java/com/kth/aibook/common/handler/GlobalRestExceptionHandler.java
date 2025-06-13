1d65c61dad400d482f857caa2d400b3228e865b9
kkkkimtaehyeon <kth1927@naver.com>
2025-06-13 16:49:07 +0900

    validation fail 에러 상세메세지 응답

--- src/main/java/com/kth/aibook/common/handler/GlobalRestExceptionHandler.java
+++ src/main/java/com/kth/aibook/common/handler/GlobalRestExceptionHandler.java
@@ -6,8 +6,12 @@ import com.kth.aibook.dto.common.ApiResponseBody;
 import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalRestExceptionHandler {
@@ -22,5 +26,13 @@ public class GlobalRestExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponseBody(e.getMessage(), null));
     }
 
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponseBody> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        StringBuilder errorDetailMessage = new StringBuilder();
+        List<ObjectError> errors = e.getBindingResult().getAllErrors();
+        for (ObjectError error : errors) {
+            errorDetailMessage.append(error.getDefaultMessage()).append("\n");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponseBody(errorDetailMessage.toString(), null));
+    }
 }
