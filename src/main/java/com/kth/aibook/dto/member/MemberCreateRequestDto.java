872d1ad4d2c5d4226c3bd5d4cbf19e8bec190e0a
kkkkimtaehyeon <kth1927@naver.com>
2025-06-13 16:49:16 +0900

    validation fail 에러 상세메세지 설정

--- src/main/java/com/kth/aibook/dto/member/MemberCreateRequestDto.java
+++ src/main/java/com/kth/aibook/dto/member/MemberCreateRequestDto.java
@@ -2,6 +2,7 @@ package com.kth.aibook.dto.member;
 
 import com.kth.aibook.entity.member.Member;
 import com.kth.aibook.entity.member.MemberRole;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -15,20 +16,21 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 public class MemberCreateRequestDto {
-    @Email(message = "이메일은 필수값입니다.")
+    @NotBlank(message = "이메일은 필수값입니다.")
+    @Email(message = "잘못된 이메일 형식입니다.")
     private String email;
 
     @NotBlank(message = "닉네임은 필수값입니다.")
-    @Length(min = 2, max = 12)
+    @Length(min = 2, max = 12, message = "닉네임은 2~12 자리 사이입니다.")
     private String nickName;
 
-    @NotNull
+    @NotNull(message = "생년월일은 필수값입니다.")
     private LocalDate birthDate;
 
-    @NotBlank
+    @Nullable
     private String oauthProvider;
 
-    @NotNull
+    @Nullable
     private Long oauthProviderMemberId;
 
     public Member toEntity() {
