055b11b3b4dcec5140ee4336214bbc0ae2ddf822
kkkkimtaehyeon <kth1927@naver.com>
2025-06-17 14:20:58 +0900

    s3 파일 업로드 메서드 예외 처리

--- src/main/java/com/kth/aibook/common/handler/GlobalRestExceptionHandler.java
+++ src/main/java/com/kth/aibook/common/handler/GlobalRestExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.kth.aibook.common.handler;
 
+import com.kth.aibook.common.exception.FileUploadException;
 import com.kth.aibook.common.exception.JwtExpiredException;
 import com.kth.aibook.dto.common.ApiResponseBody;
 import io.jsonwebtoken.JwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalRestExceptionHandler {
 
@@ -33,5 +36,13 @@ public class GlobalRestExceptionHandler {
             errorDetailMessage.append(error.getDefaultMessage()).append("\n");
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponseBody(errorDetailMessage.toString(), null));
+    }
+
+    @ExceptionHandler(FileUploadException.class)
+    public ResponseEntity<ApiResponseBody> handleFileUploadException(FileUploadException e) {
+        log.error("파일 업로드 오류: {}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ApiResponseBody("파일 업로드 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.", null));
     }
 }
